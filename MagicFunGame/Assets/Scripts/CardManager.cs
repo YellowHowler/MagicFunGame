77676cb7a5d71b1e904d2098a90889dd9d25aeb1
tenor-ow <76924426+tenor-ow@users.noreply.github.com>
2022-08-04 15:10:58 -0400

    .

--- MagicFunGame/Assets/Scripts/CardManager.cs
+++ MagicFunGame/Assets/Scripts/CardManager.cs
@@ -183,7 +183,12 @@ public class CardManager : MonoBehaviour
     {
         
     }
-
+    public void getShootWater()
+    {
+        StartCoroutine(ShootWater());
+    
+    
+    }
     private void OnActivated()
     {
         childRend.material.color = new Color(0, 1, 0, 1);
