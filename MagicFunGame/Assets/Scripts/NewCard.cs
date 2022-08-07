127fbe0e36bf73bfa213e0c6430f459303b15e2f
tenor-ow <76924426+tenor-ow@users.noreply.github.com>
2022-08-07 14:52:18 -0400

    .

---
+++ MagicFunGame/Assets/Scripts/NewCard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCard : MonoBehaviour
+{
+    [SerializeField] GameObject fireCard;
+    [SerializeField] GameObject waterCard;
+    [SerializeField] GameObject earthCard;
+    [SerializeField] GameObject windCard;
+    
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
