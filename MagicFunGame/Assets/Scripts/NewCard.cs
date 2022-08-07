343954367c02e6bb3a5e56dc86b884cd2513833c
tenor-ow <76924426+tenor-ow@users.noreply.github.com>
2022-08-07 16:38:56 -0400

    .

--- MagicFunGame/Assets/Scripts/NewCard.cs
+++ MagicFunGame/Assets/Scripts/NewCard.cs
@@ -8,7 +8,15 @@ public class NewCard : MonoBehaviour
     [SerializeField] GameObject waterCard;
     [SerializeField] GameObject earthCard;
     [SerializeField] GameObject windCard;
-    
+    [SerializeField] cardType thisCard;
+    enum cardType
+    {
+        fire,
+        water,
+        earth,
+        wind
+
+    }
     // Start is called before the first frame update
     void Start()
     { 
@@ -18,6 +26,25 @@ public class NewCard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GetComponent<CardManager>().isSelected)
+        {
+            if (thisCard == cardType.fire)
+            {
+                Instantiate(fireCard);
+            }
+            else if (thisCard == cardType.earth)
+            {
+                Instantiate(earthCard);
+            }
+            else if (thisCard == cardType.water)
+            {
+                Instantiate(waterCard);
+            }
+            else if (thisCard == cardType.wind)
+            {
+                Instantiate(windCard);
+            }
+
+        }
     }
 }
